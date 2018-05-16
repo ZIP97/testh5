ca0e472ef69e89c76a1e5aa488df9b33518e1f32
ZJPzjp <1137967378@qq.com>
2018-05-16 21:17:37 +0800

    a test for R code

---
+++ 5.R
@@ -0,0 +1,13 @@
+# Title     :  test
+# Objective :  by ZJP
+# Created by: Administrator
+# Created on: 2018/5/16
+
+a <- rnorm(10)
+b <- rnorm(10)
+print("sin (a^2)")
+print(sin(a*a))
+print("a")
+print(a)
+
+plot(a, b)
